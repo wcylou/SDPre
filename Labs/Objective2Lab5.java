a16f46400b81dda2301c5b1ec6ae4ba654a8442b
Wilson <wilson_lou@hotmail.co.uk>
2018-04-09 14:38:41 +0200

    complete Objective2Lab5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,13 @@
+public class Objective2Lab5 {
+  public static void main(String[]args) {
+
+    double side1, side2, hypotenuse;
+
+    side1 = 8;
+    side2 = 6;
+    hypotenuse = Math.sqrt((side1*side1) + (side2*side2));
+
+    System.out.println("The hypotenuse of a tringle with sides " + side1 + " and " + side2 + " is " + hypotenuse);
+  
+  }
+}
