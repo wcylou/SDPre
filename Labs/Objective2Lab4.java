f68281963eded667bfd9acb4af76a51a8e60a813
Wilson <wilson_lou@hotmail.co.uk>
2018-04-09 14:20:10 +0200

    complete Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,13 @@
+public class Objective2Lab4 {
+  public static void main(String[]args) {
+  double num1, num2, num3, average;
+
+  num1 = 2;
+  num2 = 4;
+  num3 = 6;
+  average = (num1 + num2 + num3) / 3;
+
+  System.out.println(average);
+
+  }
+}
