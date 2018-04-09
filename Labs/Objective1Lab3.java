da7e25e3b90455a523e3de08999a88f20807fce8
Wilson <wilson_lou@hotmail.co.uk>
2018-04-09 13:39:22 +0200

    complete Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args){
+    System.out.println("Name: Wilson Lou");
+    System.out.println("Age: 25");
+    System.out.println("State: London");
+  }
+}
