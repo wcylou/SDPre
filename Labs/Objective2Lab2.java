75991cd62c820274f07574c03ad566999a060f8d
Wilson <wilson_lou@hotmail.co.uk>
2018-04-09 14:02:11 +0200

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Wilson Lou";
+    int age = 25;
+    String state = "London";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
