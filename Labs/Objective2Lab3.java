e994efb47e2af1dc62d6f489c1f95b4f5c08ff6e
Wilson <wilson_lou@hotmail.co.uk>
2018-04-09 14:10:20 +0200

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[]args) {
+    String food1 = "pizza";
+    String food2 = "mexican";
+    String food3 = "thai";
+
+    System.out.println("My favourite foods are:");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
+  }
+}
