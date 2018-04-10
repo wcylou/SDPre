ee5f9cf2417a1bf95c6174fdc8fb5549334cba64
Wilson <wilson_lou@hotmail.co.uk>
2018-04-10 18:09:06 +0200

    complete Objective4Lab2

---
+++ Labs/Objective4Lab2.java
@@ -0,0 +1,24 @@
+public class Objective4Lab2 {
+  public static void main(String[]args) {
+    String string1 = "Banana";
+    String string2 = "Bacon";
+    String string3 = "Banana";
+
+    if (string1.equals(string2)) {
+      System.out.println(string1 + " is equivalent to " + string2);
+    }
+
+    else {
+      System.out.println(string1 + " is not equivalent to " + string2);
+    }
+
+    if (string1.equals(string3)) {
+      System.out.println(string1 + " is equal to " + string3);
+    }
+
+    else {
+      System.out.println(string1 + " is not equal to " + string3);
+    }
+
+  }
+}
