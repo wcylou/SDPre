796c00245b80a5e675d6d4c95a4a2945b3b7a7c7
Wilson <wilson_lou@hotmail.co.uk>
2018-04-12 11:08:31 +0100

    complete ex33

---
+++ JavaTheHardWay/ThereAndBackAgain.java
@@ -0,0 +1,12 @@
+public class ThereAndBackAgain {
+  public static void main(String[] args) {
+    System.out.println("Here.");
+    erebor();
+    System.out.println("Here 2.");
+    erebor();
+    System.out.println("Here 3.");
+  }
+ public static void erebor() {
+   System.out.println("There");    
+ }
+}
