857cacf92a731198f9fed4a628f44debbe87f940
Wilson <wilson_lou@hotmail.co.uk>
2018-04-12 11:41:23 +0100

    complete Objective6Lab1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,11 @@
+public class Objective6Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while (counter > 0) {
+      System.out.println(counter);
+
+      counter = counter -1; //this counts down
+    }
+  }
+}
