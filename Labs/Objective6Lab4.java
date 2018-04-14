60bbe90f1d299be45bdefc121b48ed62171967d3
Wilson <wilson_lou@hotmail.co.uk>
2018-04-14 22:17:12 +0100

    complete Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4 {
+  public static void main(String[]args) {
+    int sum = 0, counter = 1;
+
+    while (counter < 21) {
+      sum += counter;
+      counter = counter + 1;
+    }
+    System.out.println(sum);
+  }
+}
