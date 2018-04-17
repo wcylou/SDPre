289144b0428b15ccf3a1141648d3cf506febec1f
Wilson <wilson_lou@hotmail.co.uk>
2018-04-17 11:17:12 +0100

    complete Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Objective7Lab3 {
+  public static void main(String[]args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true) {
+      printMenu();
+      selection = scanner.nextInt();
+      if (selection == 1) {
+        System.out.println("Hello Human");
+      }
+      else if (selection == 2) {
+        System.out.println("Apples");
+      }
+      else {
+        System.out.println("Goodbye");
+        break;
+      }
+
+    }
+    scanner.close();
+  }
+
+  public static void printMenu() {
+    System.out.println("___Menu___");
+    System.out.println("1. Say Hello");
+    System.out.println("2. List My Favourite Foods");
+    System.out.println("3. Exit");
+    System.out.println();
+  }
+}
